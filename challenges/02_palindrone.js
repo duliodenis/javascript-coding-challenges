292e09386c27e4c0d88e8e62a3e2a9e6f9642aa4
Dulio Denis <dulio.denis@yahoo.com>
2018-02-19 16:31:47 -0500

    Added a Palindrone solution using the every method of array

--- challenges/02_palindrone.js
+++ challenges/02_palindrone.js
@@ -4,8 +4,32 @@ function isPalindrone(str) {
     return (str === str.split('').reverse().join(''));
 }
 
+function isPalindroneWhiteSpace(str) {
+    strNoSpace = str.toLowerCase().replace(/ /g, '');
+    return (strNoSpace === strNoSpace.split('').reverse().join(''));
+}
+
+
+// Uses the array every method
+
+function isPalindroneEvery(str) {
+    return str.split('').every( (char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
+
+console.log("abba is:");
 palindrone0 = isPalindrone("abba");
 console.log(palindrone0);
 
+console.log("Never odd or even");
 palindrone1 = isPalindrone("Never odd or even");
 console.log(palindrone1);
+
+console.log("Never odd or even with whitespace and lower cased: ");
+palindrone1B = isPalindroneWhiteSpace("Never odd or even");
+console.log(palindrone1B);
+
+console.log("abcba");
+palindrone2 = isPalindroneEvery("abcba");
+console.log(palindrone2);   
